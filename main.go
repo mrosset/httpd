524271a798dca7dbd3a7b85acd2838f4680893a9
mike rosset <mike.rosset@gmail.com>
2015-10-24 17:22:57 -0700

    handle php files

--- main.go
+++ main.go
@@ -6,7 +6,8 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"path"
+	"os/exec"
+	"path/filepath"
 )
 
 type Config struct {
@@ -21,7 +22,35 @@ var (
 )
 
 func init() {
-	err := json.Read(config, path.Join(home, ".httpd.json"))
+	err := json.Read(config, filepath.Join(home, ".httpd.json"))
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func Root(w http.ResponseWriter, r *http.Request) {
+	path := "." + r.URL.Path
+	switch path {
+	case "./":
+		path = "./index.php"
+	}
+	fmt.Println(path)
+	if filepath.Ext(path) == ".php" {
+		phpHandle(w, r, path)
+		return
+	}
+	http.ServeFile(w, r, path)
+}
+
+func phpHandle(w http.ResponseWriter, r *http.Request, path string) {
+	fd, err := os.Open(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	php := exec.Command("php")
+	php.Stdin = fd
+	php.Stdout = w
+	err = php.Run()
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -29,11 +58,20 @@ func init() {
 
 func main() {
 	var (
-		host    = fmt.Sprintf("%s:%s", config.Host, config.Port)
-		handler = http.FileServer(http.Dir(config.Root))
+		host = fmt.Sprintf("%s", config.Host)
+		root = config.Root
 	)
-	fmt.Printf("staring http://%s:%s/\n", config.Host, config.Port)
-	err := http.ListenAndServe(host, handler)
+	if root == "" {
+		root = "."
+	}
+	err := os.Chdir(root)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(os.Getwd())
+	fmt.Printf("staring http://%s/\n", host)
+	http.HandleFunc("/", Root)
+	err = http.ListenAndServe(host, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
